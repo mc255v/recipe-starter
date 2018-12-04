2bcaa5b967ff9f199372c986064c4244d3da887d
Matthew Cook <mc255v@gmail.com>
2018-12-04 13:42:01 +0900

    added navbar and recipe list component

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Recipe from './Recipe';
+import Navbar from './Navbar';
+import RecipeList from './RecipeList';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Recipe
-            title="pasta"
-            ingredients={['flour', 'water']}
-            instructions="Mix ingredients"
-            img="spaghetti.jpg"
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Navbar/>
+                <RecipeList/>
+            </div>
+        );
+    }
 }
 
 export default RecipeApp;
